c0afa5029e030a77d8c31cf886b98381627efdbb
Raphael Inácio da Silva <contato.raphaelinacio@gmail.com>
2018-11-13 15:33:23 -0200

    Impl find sequence dna in horizontal - Added unit test - Create method to find occurrences in horizontal - Added constant to base number gene occorrencies

--- src/main/java/com/mercadolibre/ismutant/service/MutantAnalyzeServiceImpl.java
+++ src/main/java/com/mercadolibre/ismutant/service/MutantAnalyzeServiceImpl.java
@@ -10,19 +10,32 @@ import java.util.regex.Pattern;
 @Service
 public class MutantAnalyzeServiceImpl implements MutantAnalyzeService {
 
-    private static final List<String> VALID_GENES = Arrays.asList("A", "T", "C", "G");
     private static final Pattern dnaMatchPattern = Pattern.compile("(C{4}|A{4}|T{4}|G{4})");
     private static final Pattern noDnaMatch = Pattern.compile("(?!A|C|T|G)([A-Za-z0-9!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?])");
+    private static final long baseNumberSequenceGene = 1;
 
     @Override
     public boolean isMutant(String[] dna) throws AnalyzeMutantException {
         List<String> dnaGenes = Arrays.asList(dna);
-        containsInValidGenes(dnaGenes);
-        return Boolean.TRUE;
+        validateInputGenes(dnaGenes);
+        return getOccurrencesInHorizontal(dnaGenes) > baseNumberSequenceGene;
     }
 
-    private void containsInValidGenes(List<String> dna) {
-        dna.stream().filter(item -> noDnaMatch.matcher(item).find()).findFirst().ifPresent(s -> {
+    private long getOccurrencesInHorizontal(List<String> dna) {
+        return dna.stream().filter(gene -> dnaMatchPattern.matcher(gene).find()).count();
+    }
+
+    private void validateInputGenes(List<String> dna) {
+
+        if (dna.isEmpty()) {
+            throw new IllegalArgumentException("Invalid fields founded");
+        }
+
+        containInValidGenes(dna);
+    }
+
+    private void containInValidGenes(List<String> dna) {
+        dna.stream().filter(gene -> noDnaMatch.matcher(gene).find()).findFirst().ifPresent(s -> {
             throw new IllegalArgumentException("Invalid fields founded");
         });
     }
--- src/test/java/com/mercadolibre/ismutant/service/MutantAnalyzeServiceImplTest.java
+++ src/test/java/com/mercadolibre/ismutant/service/MutantAnalyzeServiceImplTest.java
@@ -13,4 +13,49 @@ public class MutantAnalyzeServiceImplTest {
         mutantAnalize.isMutant(new String[] { "AAAA", "CCCC", "TCAC10(*&@#", "123123" });
     }
 
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldTrowIllegalArgumentExceptionWhenInvalidDNAEmpty() throws AnalyzeMutantException {
+        mutantAnalize.isMutant(new String[] {});
+    }
+
+    @Test
+    public void shouldReturnFalseWhenSequenceAIsMinorThanFourOccurrences() throws AnalyzeMutantException {
+        Assert.assertFalse(mutantAnalize.isMutant(new String[] { "AAA" }));
+    }
+
+    @Test
+    public void shouldReturnFalseWhenSequenceCIsMinorThanFourOccurrences() throws AnalyzeMutantException {
+        Assert.assertFalse(mutantAnalize.isMutant(new String[] { "CCC" }));
+    }
+
+    @Test
+    public void shouldReturnFalseWhenSequenceTIsMinorThanFourOccurrences() throws AnalyzeMutantException {
+        Assert.assertFalse(mutantAnalize.isMutant(new String[] { "TTT" }));
+    }
+
+    @Test
+    public void shouldReturnFalseWhenSequenceGIsMinorThanFourOccurrences() throws AnalyzeMutantException {
+        Assert.assertFalse(mutantAnalize.isMutant(new String[] { "GG" }));
+    }
+
+    @Test
+    public void shouldReturnTrueWhenSequenceAIsFindInHorizontalGraterThanOneOccurrence() throws AnalyzeMutantException {
+        Assert.assertTrue(mutantAnalize.isMutant(new String[] { "AAAA", "AAAA" }));
+    }
+
+    @Test
+    public void shouldReturnTrueWhenSequenceCIsFindInHorizontalGraterThanOneOccurrence() throws AnalyzeMutantException {
+        Assert.assertTrue(mutantAnalize.isMutant(new String[] { "CCCC", "CCCC" }));
+    }
+
+    @Test
+    public void shouldReturnTrueWhenSequenceTIsFindInHorizontalGraterThanOneOccurrence() throws AnalyzeMutantException {
+        Assert.assertTrue(mutantAnalize.isMutant(new String[] { "TTTT", "TTTT" }));
+    }
+
+    @Test
+    public void shouldReturnTrueWhenSequenceGIsFindInHorizontalGraterThanOneOccurrence() throws AnalyzeMutantException {
+        Assert.assertTrue(mutantAnalize.isMutant(new String[] { "GGGG", "GGGG" }));
+    }
+
 }
