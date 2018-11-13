02861f457a40684a6399a7eccd180fbcc5b0f5ce
Raphael Inácio da Silva <contato.raphaelinacio@gmail.com>
2018-11-13 13:02:27 -0200

    Verification for invalid dna - Created regex to find invalid sequence - Create Service class - Create unit test to test invalid sequence

--- src/main/java/com/mercadolibre/ismutant/service/MutantAnalyzeServiceImpl.java
+++ src/main/java/com/mercadolibre/ismutant/service/MutantAnalyzeServiceImpl.java
@@ -1,21 +1,29 @@
 package com.mercadolibre.ismutant.service;
 
 import com.mercadolibre.ismutant.service.exception.AnalyzeMutantException;
+import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Pattern;
 
+@Service
 public class MutantAnalyzeServiceImpl implements MutantAnalyzeService {
 
     private static final List<String> VALID_GENES = Arrays.asList("A", "T", "C", "G");
+    private static final Pattern dnaMatchPattern = Pattern.compile("(C{4}|A{4}|T{4}|G{4})");
+    private static final Pattern noDnaMatch = Pattern.compile("(?!A|C|T|G)([A-Za-z0-9!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?])");
 
     @Override
     public boolean isMutant(String[] dna) throws AnalyzeMutantException {
         List<String> dnaGenes = Arrays.asList(dna);
-        isValidGenes(dnaGenes);
-        return Boolean.FALSE;
+        containsInValidGenes(dnaGenes);
+        return Boolean.TRUE;
     }
 
-    private void isValidGenes(List<String> dna) throws AnalyzeMutantException {
+    private void containsInValidGenes(List<String> dna) {
+        dna.stream().filter(item -> noDnaMatch.matcher(item).find()).findFirst().ifPresent(s -> {
+            throw new IllegalArgumentException("Invalid fields founded");
+        });
     }
 }
--- src/test/java/com/mercadolibre/ismutant/service/MutantAnalyzeServiceImplTest.java
+++ src/test/java/com/mercadolibre/ismutant/service/MutantAnalyzeServiceImplTest.java
@@ -8,14 +8,9 @@ public class MutantAnalyzeServiceImplTest {
 
     private MutantAnalyzeServiceImpl mutantAnalize = new MutantAnalyzeServiceImpl();
 
-    @Test(expected = AnalyzeMutantException.class)
-    public void shouldTrowAnalizeExceptionWhenInvalidDNAisFinded() throws AnalyzeMutantException {
-        mutantAnalize.isMutant(new String[]{"AGAAGG", "AGAAGA"});
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldTrowIllegalArgumentExceptionWhenInvalidDNAisFinded() throws AnalyzeMutantException {
+        mutantAnalize.isMutant(new String[] { "AAAA", "CCCC", "TCAC10(*&@#", "123123" });
     }
 
-    @Test
-    public void shouldFindMutandWhenDNAisFindedInHorizontalSend() throws AnalyzeMutantException {
-        boolean mutant = mutantAnalize.isMutant(new String[]{"ATGCGA", "CAGTGC", "TTATGT", "AGAAGG", "CCCCTA", "TCACTG"});
-        Assert.assertTrue(mutant);
-    }
 }
