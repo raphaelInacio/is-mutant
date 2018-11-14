84fbdadfd2b66b47a26213b09ad4ceb0d7e81482
Raphael <contato.raphaelinacio@gmail.com>
2018-11-14 08:10:26 -0200

    Creation unit test to for API

--- src/main/java/com/mercadolibre/ismutant/IsMutantApplication.java
+++ src/main/java/com/mercadolibre/ismutant/IsMutantApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class IsMutantApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(IsMutantApplication.class, args);
 	}
--- src/main/java/com/mercadolibre/ismutant/controller/MutantAnalyzeController.java
+++ src/main/java/com/mercadolibre/ismutant/controller/MutantAnalyzeController.java
@@ -6,13 +6,10 @@ import com.mercadolibre.ismutant.service.MutantAnalyzeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/mutant")
@@ -22,7 +19,7 @@ public class MutantAnalyzeController {
     private MutantAnalyzeService analyzeService;
 
     @PostMapping
-    public ResponseEntity<Void> isMutant(@RequestBody @Valid Human human) throws AnalyzeMutantException {
+    public ResponseEntity<Void> isMutant(@RequestBody Human human) throws AnalyzeMutantException {
         if (analyzeService.isMutant(human.getDna())) {
             return new ResponseEntity<>(HttpStatus.OK);
         }
